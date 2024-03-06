ba23834cfb7d14e5b292032dec81bf796af77875
sabrieker <sabri.eker@racoon-development.at>
2024-03-06 19:51:05 +0100

    google map api implementation start coordianates uas drone technologies

--- tools/py_files/widgets/zequentmapview.py
+++ tools/py_files/widgets/zequentmapview.py
@@ -9,19 +9,25 @@ class ZequentMapView(MapView):
 
    
     app=MDApp.get_running_app()
-    latitude = 48
-    longitude = 48
+    latitude = 47.28876860219628
+    longitude = 11.14698035747897
    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.zoom=20
-        apiKey = 'c7b23514f42f4878b7a8397f7ecfdef5'
-        source = MapSource(url='https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey='+apiKey,
-                            cache_key="thunderforest-map", tile_size=512,
-                            image_ext="png", attribution="@ThunderForest")
-        self.map_source.from_provider("thunderforest-cycle")
+        self.zoom=17
+        self.lat = 47.28876860219628
+        self.lon = 11.14698035747897
+
+
+
+        apiKey = 'AIzaSyBSwt7u9Pn-Hw09bWsiQ-ZQqlE7aGf5Gxg'
+        session = 'AJVsH2xGGYNwrRk_cn8hF5AKWVbN527eF2s3013IJJT9WZWRANmAVTT_ZIz5IFzRQYrkH6oDdo2Zc4E9QJwkl8ih_w'
+        source = MapSource(url='https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?key='+apiKey + '&session=' + session,
+                            cache_key="google-maps", tile_size=256,
+                            image_ext="jpeg", attribution="@GoogleMaps")
+       # self.map_source.from_provider("thunderforest-cycle")
         self.map_source = source
-        self.updateMap()
+        #self.updateMap()
         
     
     def updateMap(self):
@@ -34,11 +40,7 @@ class ZequentMapView(MapView):
             
         self.center_on(latitude, longitude)
 
-    def on_touch_down(self, touch):
-        self.app=MDApp.get_running_app()
-        print(self.lat)
-        self.updateMap()
-        return super().on_touch_down(touch)
+
     
     def build(self):
         pass
