fb7806ee442ed609766b2d792513da29492b2eb8
sabrieker <s.eker@zequent.com>
2024-03-11 20:29:29 +0100

    adding every method to Utils class

--- tools/Utils.py
+++ tools/Utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time, traceback
 
 PROJECT_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
 
@@ -43,3 +44,18 @@ class Utils:
                     
         
         return navigationItems
+        
+
+    @staticmethod
+    def every(delay, task):
+        next_time = time.time() + delay
+        while True:
+            time.sleep(max(0, next_time - time.time()))
+            try:
+                task()
+            except Exception:
+                traceback.print_exc()
+            # in production code you might want to have this instead of course:
+            # logger.exception("Problem while executing repetitive task.")
+            # skip tasks if we are behind schedule:
+            next_time += (time.time() - next_time) // delay * delay + delay
--- tools/py_files/layouts/maincontrollerlayout.py
+++ tools/py_files/layouts/maincontrollerlayout.py
@@ -6,7 +6,7 @@ from tools.Utils import *
 from kivymd.app import MDApp
 from zequentmavlinklib.ArduPlane import ArduPlaneObject
 import threading
-import time
+from tools.Utils import *
 
 from tools.py_files.layouts.zequentcameralayout import ZequentCameraLayout
 class MainControllerLayout(ZequentBoxLayout):
@@ -19,7 +19,7 @@ class MainControllerLayout(ZequentBoxLayout):
         self.drone: ArduPlaneObject =self.app.drone
         #thread = threading.Thread(target=self.test2)
         #thread. threading.Timer(1, self.test).start()
-        threading.Thread(target=lambda: self.every(1, self.test)).start()
+        threading.Thread(target=lambda: Utils.every(.1, self.test)).start()
        # thread.start()
         #thread.join()
         print("fuck off thread is finished")
@@ -28,20 +28,6 @@ class MainControllerLayout(ZequentBoxLayout):
 
     def build(self):
         pass
-
-
-    def every(self, delay, task):
-        next_time = time.time() + delay
-        while True:
-            time.sleep(max(0, next_time - time.time()))
-            try:
-                task()
-            except Exception:
-                traceback.print_exc()
-            # in production code you might want to have this instead of course:
-            # logger.exception("Problem while executing repetitive task.")
-            # skip tasks if we are behind schedule:
-            next_time += (time.time() - next_time) // delay * delay + delay
 
 
     def test(self):
