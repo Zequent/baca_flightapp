fba0d4dd053c7545fdce28d5b028f00b97e08ade
sabrieker <sabri.eker@racoon-development.at>
2024-03-27 07:38:37 +0100

    refactoring

--- tools/py_files/layouts/zequentconnectionlayout.py
+++ tools/py_files/layouts/zequentconnectionlayout.py
@@ -28,6 +28,7 @@ class ZequentConnectionLayout(ZequentGridLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.drone = None
         self.app = MDApp.get_running_app()
         if self.app.root is not None:
             self.connectionStatusText = self.app.root.ids.translator.translate('not_connected')
@@ -38,57 +39,57 @@ class ZequentConnectionLayout(ZequentGridLayout):
     def execute_connect(self):
         return self.drone.connect()
 
-    def tryConnection(self, *args):
+    def try_connection(self, *args):
 
         button: ZequentButton = self.ids.connect_button
         connectionType: ZequentGridLayout = self.ids.connection_type
-        currStateLabel: ZequentLabel = self.ids.connection_status_label
+        curr_state_label: ZequentLabel = self.ids.connection_status_label
 
         self.app = MDApp.get_running_app()
-        if self.ids.rfc_button.disabled == False:
+        if not self.ids.rfc_button.disabled:
             self.app.log.info("RFC")
-        elif self.ids.lte_button.disabled == False:
-            lteAddress = self.ids.lte_address
-            lteAddress = str(lteAddress.text)
-            if lteAddress is None or lteAddress is "":
+        elif not self.ids.lte_button.disabled:
+            lte_address = self.ids.lte_address
+            lte_address = str(lte_address.text)
+            if lte_address is None or lte_address is "":
                 ZequentToast.showInfoMessage(self.app.root.ids.translator.translate('lte_address_input_invalid'))
                 return
             else:
-                # log.info("LTE adress:"+ lteAddress)
+                # log.info("LTE adress:"+ lte_address)
                 connectionType: ZequentDropDownItem = self.ids.lte_connection_type
                 # log.info(connectionType.current_item)
-                # log.info(lteAddress)
+                # log.info(lte_address)
                 self.drone = ArduPlaneObject("TestVtol", "testuuid", "OrgId", "TestModel",
-                                             ConnectionType.UDPIN, lteAddress, "14550", None)
-                connectionResponse = self.drone.connect()
+                                             ConnectionType.UDPIN, lte_address, "14550", None)
+                connection_response = self.drone.connect()
 
-        if isinstance(connectionResponse, ErrorMessage):
+        if isinstance(connection_response, ErrorMessage):
             GraphicalChangeExecutor.execute(self.remove_spinner)
-            connectionResponse: ErrorMessage
-            currStateLabel.text = self.app.root.ids.translator.translate('failed_message')
-            currStateLabel.color = self.app.customColors["failure"]
-            ZequentToast.showErrorMessage(connectionResponse.message)
+            connection_response: ErrorMessage
+            curr_state_label.text = self.app.root.ids.translator.translate('failed_message')
+            curr_state_label.color = self.app.customColors["failure"]
+            ZequentToast.showErrorMessage(connection_response.message)
             GraphicalChangeExecutor.execute(self.enable_widgets)
         else:
-            connectionResponse: MAVLink_heartbeat_message
+            connection_response: MAVLink_heartbeat_message
             self.app.set_drone_instance(self.drone)
             button.disabled = True
-            currStateLabel.text = self.app.root.ids.translator.translate('success_message')
-            currStateLabel.color = self.app.customColors["success"]
+            curr_state_label.text = self.app.root.ids.translator.translate('success_message')
+            curr_state_label.color = self.app.customColors["success"]
             self.app.set_vehicle_type(str(self.ids.vehicle_item.current_item))
             Clock.schedule_once(partial(self.app.changeScreen, 'main'), 3)
 
     def add_spinner(self, button):
         connection_grid: ZequentGridLayout = self.ids.connection_grid
-        anchorLayout = ZequentAnchorLayout()
+        anchor_layout = ZequentAnchorLayout()
         spinner = ZequentSpinner()
         spinner.opacity = 1
-        anchorLayout.add_widget(spinner)
-        connection_grid.add_widget(anchorLayout)
-        self.ids['anchor_layout_spinner'] = weakref.ref(anchorLayout)
+        anchor_layout.add_widget(spinner)
+        connection_grid.add_widget(anchor_layout)
+        self.ids['anchor_layout_spinner'] = weakref.ref(anchor_layout)
         self.disable_widgets()
 
-        thread = WorkerThread(method=self.tryConnection, name="Connecting to Vehicle")
+        thread = WorkerThread(method=self.try_connection, name="Connecting to Vehicle")
         thread.start()
         thread.join()
 
